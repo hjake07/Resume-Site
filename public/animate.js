9cfb9bfb60a2429090036e634f88bafbf77ccb57
Jacob Davies <jacobdavies@Jacobs-MacBook-Pro.local>
2023-01-17 19:39:58 -0700

    finished

---
+++ public/animate.js
@@ -0,0 +1,28 @@
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry =>{
+        if (entry.isIntersecting) {
+            entry.target.classList.add('movein-animation')
+            return;
+        }
+            entry.target.classList.remove('movein-animation')
+    })
+})
+observer.observe(document.querySelector('.movein-wrapper'))
+observer.observe(document.querySelector('.movein-wrapper1'))
+observer.observe(document.querySelector('.movein-wrapper2'))
+observer.observe(document.querySelector('.movein-wrapper3'))
+observer.observe(document.querySelector('.movein-wrapper4'))
+
+const newObserver = new IntersectionObserver(entries =>{
+    entries.forEach(entry => {
+        if(entry.isIntersecting) {
+            entry.target.classList.add('fadingtext')
+            return;
+        }
+            entry.target.classList.remove('fadingtext')
+    })
+})
+newObserver.observe(document.querySelector('.fadeInUl'))
+newObserver.observe(document.querySelector('.fadeInUl1'))
+newObserver.observe(document.querySelector('.fadeInP'))
+
