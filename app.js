68ced1a746cb0031bc2f3ab2a74068377b9d38a3
Jake <jdavies212@gmail.com>
2023-03-16 19:21:00 -0600

    added changes

--- app.js
+++ app.js
@@ -6,7 +6,22 @@ app.use(express.urlencoded({extended:false}))
 app.use(express.static('./public'))
 app.use(express.json())
 app.get('/', (req, res)=>{
-res.render('index')
+res.sendFile(__dirname +'/public/index.html')
+})
+app.get('/contact',(req,res)=>{
+res.sendFile(__dirname+"/public/contact.html")
+})
+app.get('/projects',(req,res)=>{
+res.sendFile(__dirname+"/public/projects.html")
+})
+app.get('/xceed', (req,res)=>{
+    res.sendFile(__dirname+"/public/xceed.html")
+})
+app.get('/paint',(req,res)=>{
+    res.sendFile(__dirname+"/public/paint.html")
+})
+app.get('/pokemon',(req,res)=>{
+    res.sendFile(__dirname+"/public/pokemon.html")
 })
 app.listen(port, ()=>{
 console.log('listening on port: ' + port)
