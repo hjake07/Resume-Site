d456de77acf89df2b153669be04cdd45d053ba83
Jake <jdavies212@gmail.com>
2023-05-26 08:32:13 -0600

    more info button works

--- public/animate.js
+++ public/animate.js
@@ -1,28 +1,30 @@
-const observer = new IntersectionObserver(entries => {
-    entries.forEach(entry =>{
-        if (entry.isIntersecting) {
-            entry.target.classList.add('movein-animation')
-            return;
-        }
-            entry.target.classList.remove('movein-animation')
-    })
-})
-// observer.observe(document.querySelector('.movein-wrapper'))
-// observer.observe(document.querySelector('.movein-wrapper1'))
-observer.observe(document.querySelector('.movein-wrapper2'))
-// observer.observe(document.querySelector('.movein-wrapper3'))
-// observer.observe(document.querySelector('.movein-wrapper4'))
+let showing = false;
+function toggleShowing(){
+    if(showing ===false){
+        showing = true;
+        document.getElementById("html").className = 'fadingtext';
+        document.getElementById("css").className = 'fadingtext';
+        document.getElementById("js").className = 'fadingtext';
+        document.getElementById("react").className = 'fadingtext';
+        document.getElementById("node").className = 'fadingtext';
+        document.getElementById("angular").className = 'fadingtext';
+        document.getElementById("express").className = 'fadingtext';
+        document.getElementById("typescript").className = 'fadingtext';
+        document.getElementById("php").className = 'fadingtext';
+        document.getElementById("morebutton").innerHTML = "Less Details"
 
-const newObserver = new IntersectionObserver(entries =>{
-    entries.forEach(entry => {
-        if(entry.isIntersecting) {
-            entry.target.classList.add('fadingtext')
-            return;
-        }
-            entry.target.classList.remove('fadingtext')
-    })
-})
-newObserver.observe(document.querySelector('.fadeInUl'))
-newObserver.observe(document.querySelector('.fadeInUl1'))
-newObserver.observe(document.querySelector('.fadeInP'))
-
+    }
+    else {
+        showing = false;
+        document.getElementById("html").className = 'hidden';
+        document.getElementById("css").className = 'hidden';
+        document.getElementById("js").className = 'hidden';
+        document.getElementById("react").className = 'hidden';
+        document.getElementById("node").className = 'hidden';
+        document.getElementById("angular").className = 'hidden';
+        document.getElementById("express").className = 'hidden';
+        document.getElementById("typescript").className = 'hidden';
+        document.getElementById("php").className = 'hidden';
+        document.getElementById("morebutton").innerHTML = "More Details"
+    }
+}
